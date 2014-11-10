1f71582bf5b83f234ad8326d70d9c8d904c2c614
dushi27 <dushi27@gmail.com>
2014-11-10 15:14:30 +0000

    change root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'static#home'
-
+  root 'apps#index'
   resources :apps
 
   # The priority is based upon order of creation: first created -> highest priority.
