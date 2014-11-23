be77db440970acc3e835ed4a07fe900e0a6412e7
dushi27 <dushi27@gmail.com>
2014-11-23 00:11:41 +0000

    update commenting feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
+  
 
   devise_for :users
   root 'apps#index'
-  resources :apps
+  resources :apps do
+    resources :comments
+  end
   
   get 'static/home' => 'static#home'
   
